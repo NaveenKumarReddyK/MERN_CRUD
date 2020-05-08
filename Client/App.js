34f1a14ae8f56dfe2d590f173bdc4f1f3806f276
Naveen Reddy <53139207+NaveenKumarReddyK@users.noreply.github.com>
2020-05-08 18:13:35 +0530

    Add files via upload

---
+++ Client/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import AppHeader from "./bodyComponents/AppHeader";
+
+
+class App extends React.Component {
+  render() {
+    return (
+          <AppHeader />
+    );
+  }
+}
+
+export default App;
